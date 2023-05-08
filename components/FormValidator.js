78b9c6e08b037a19f95460be41b27c24420ffcbf
Joshua Zimmerman <Joshua.Zimmerman@lexialearning.com>
2023-05-08 16:12:40 -0500

    inital regroup and commit

---
+++ components/FormValidator.js
@@ -0,0 +1,72 @@
+export default class FormValidator {
+  constructor(validationOptions, formElement) {
+    this._formSelector = ".modal__form";
+    this._inputSelector = ".modal__input";
+    this._submitButtonSelector = ".modal__button";
+    this._inactiveButtonClass = "modal__button_disabled";
+    this._inputErrorClass = "modal__input_type_error";
+    this._errorClass = "modal__error_visible";
+
+    this._form = formElement;
+
+    this._inputList = Array.from(
+      this._form.querySelectorAll(this._inputSelector)
+    );
+
+    this._saveButton = this._form.querySelector(this._submitButtonSelector);
+  }
+
+  _showInputError(inputElement) {
+    this._errorMessageElement = this._form.querySelector(
+      `#${inputElement.id}-error`
+    );
+    inputElement.classList.add(this._inputErrorClass);
+    this._errorMessageElement.textContent = inputElement.validationMessage;
+    this._errorMessageElement.classList.add(this._errorClass);
+  }
+  _hideInputError(inputElement) {
+    this._errorMessageElement = this._form.querySelector(
+      `#${inputElement.id}-error`
+    );
+    inputElement.classList.remove(this._inputErrorClass);
+    this._errorMessageElement.textContent = "";
+    this._errorMessageElement.classList.remove(this._errorClass);
+  }
+  _hasInvalidInput() {
+    return !this._inputList.every(
+      (inputElement) => inputElement.validity.valid
+    );
+  }
+  toggleButtonState(saveButton, options) {
+    if (this._hasInvalidInput()) {
+      this._saveButton.classList.add(this._inactiveButtonClass);
+      this._saveButton.disabled = true;
+      return;
+    }
+    this._saveButton.classList.remove(this._inactiveButtonClass);
+    this._saveButton.disabled = false;
+  }
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+  _setEventListeners() {
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this.toggleButtonState();
+      });
+    });
+  }
+
+  enableValidation() {
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
+}
---
+++ scripts/Card.js
@@ -0,0 +1,58 @@
+
+
+
+
+export default class Card {
+  constructor({ cardData, cardSelector, handleCardClick }) {
+    this._name = cardData.name;
+    this._link = cardData.link;
+    this._cardSelector = cardSelector;
+    this._cardTemplateInput = document.querySelector("#card-template").content.firstElementChild;
+    this._cardElement = this._cardTemplateInput.cloneNode(true);
+    this._handleCardClick = handleCardClick;
+  }
+
+  _setEventListeners() {
+    //".card__button"
+    this._likeButton = this._cardElement.querySelector(".card__button");
+    this._likeButton.addEventListener("click", () => {
+      this._handleLikeIcon();
+    });
+    //"#card-delete-button"
+    this._deleteBtn = this._cardElement.querySelector("#card-delete-button");
+    this._deleteBtn.addEventListener("click", () => {
+      this._handleDeleteBtn();
+    });
+    //image
+    this._cardElement
+      .querySelector("#card-image")
+      .addEventListener("click", () => this._handleCardClick({name: this._name, link: this._link}));
+  }
+  _handleLikeIcon() {
+    this._likeButton.classList.toggle("card__button_active");
+  }
+  _handleDeleteBtn() {
+    this._cardElement.remove();
+    this._cardElement = null;
+  }
+
+
+  _getTemplate() {
+    return document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+  }
+
+  getCard() {
+    this._cardElement = this._getTemplate();
+    //get the card view
+    this._cardElement.querySelector("#card-image").src = this._link;
+    this._cardElement.querySelector("#card-title").textContent = this._name;
+    this._cardElement.querySelector("#card-image").alt = this._name;
+    //set the event listeners
+    this._setEventListeners();
+    //return the card
+    return this._cardElement;
+  }
+}
